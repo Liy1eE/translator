cdfb070f78d9a0779a8d54d39f06b6ac82742270
home <stb@qq.com>
2017-10-06 00:48:13 +0800

    add bat command

---
+++ clean.bat
@@ -0,0 +1,3 @@
+echo off
+rmdir /s/q translator_release
+del translator_release.*
--- copy.bat
+++ copy.bat
@@ -1,7 +1,6 @@
-echo on
+echo off
 mkdir translator_release\icons
 copy .\*.js .\translator_release\
 copy .\*.json .\translator_release\
 copy .\*.css .\translator_release\
 copy .\icons\icon.png .\translator_release\icons
-pause
