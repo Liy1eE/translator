66af48a49fbf541e81631baa410563eeb52649a1
home <stb@qq.com>
2017-10-06 00:32:30 +0800

    add bat command

---
+++ copy.bat
@@ -0,0 +1,7 @@
+echo on
+mkdir translator_release\icons
+copy .\*.js .\translator_release\
+copy .\*.json .\translator_release\
+copy .\*.css .\translator_release\
+copy .\icons\icon.png .\translator_release\icons
+pause
